5cd4a8b25cd06edb27523158917519bd039fdbb1
Dev-Leandr0 <leandroalegregabriel@outlook.com>
2025-09-26 20:38:32 -0300

    feat: cargar usuarios iniciales en la base de datos

--- src/db/database.js
+++ src/db/database.js
@@ -1,3 +1,7 @@
-const users = [];
+const users = [
+  { id: 1, name: 'Thomas', username: 'Tom', email: 'Thomas@gmail.com' },
+  { id: 2, name: 'Leandro', username: 'Lean', email: 'Leandro@gmail.com' },
+  { id: 3, name: 'Noelia', username: 'Noe', email: 'Noe@gmail.com' },
+];
 
 module.exports = users;
