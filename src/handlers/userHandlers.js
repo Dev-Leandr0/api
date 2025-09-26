160dee5f4e9daa06768d57c6b9d3427b750bcbb6
Dev-Leandr0 <leandroalegregabriel@outlook.com>
2025-09-26 20:40:24 -0300

    refactor: agregar bloques try/catch e importar controladores en los handlers

--- src/handlers/userHandlers.js
+++ src/handlers/userHandlers.js
@@ -1,27 +1,65 @@
-const { createUserController } = require("../controllers/usersControllers");
+const { createUserController, getAllUsersController, getUsersByNameController, getOneUserByIdController, updateUserController, deleteUserController } = require("../controllers/usersControllers");
+
+const Joi = require('joi');
+
+const userSchema = Joi.object({
+  name: Joi.string().min(3).required(),
+  username: Joi.string().min(3).required(),
+  email: Joi.string().email().required()
+});
+
+const createUserHandler = (req, res) => {
+  try {
+
+    const { error } = userSchema.validate(req.body);
+    if (error) res.status(404).send(error.details[0].message);
+
+    const { name, username, email } = req.body;
+    const response = createUserController(name, username, email);
+    res.status(201).send(response);
+  } catch (error) {
+    res.status(404).send({ Error: error.message });
+  }
+}
 
 const getAllUserHandler = (req, res) => {
-  const { name } = req.query;
-  (name) ? res.send(`Traer el usuario ${name}`) : res.send('Traer los usuarios');
+  try {
+    const { name } = req.query;
+    if (name) {
+      const response = getUsersByNameController(name);
+      res.status(200).send(response);
+    } else {
+      const response = getAllUsersController();
+      res.status(200).send(response);
+    }
+  } catch (error) {
+    res.status(404).send({ Error: error.message });
+  }
+
 }
 
 const getOneUserHandler = (req, res) => {
-  const { id } = req.params;
-  res.send(`Tráeme el usuario con id ${id}`);
-}
+  try {
+    const { id } = req.params;
+    const response = getOneUserByIdController(id);
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(404).send({ Error: error.message });
+  }
 
-const createUserHandler = (req, res) => {
-  const { name, username, email } = req.body;
-  const response = createUserController(name, username, email);
-  res.status(201).send(`${name} - ${username} - ${email}`);
 }
 
 const updateUserHandler = (req, res) => {
-  res.send('Usuario modificado');
+  const { id } = req.params;
+  const { name, username, email } = req.body;
+  const response = updateUserController(id, name, username, email);
+  res.status(200).send(response);
 }
 
 const deleteUserHandler = (req, res) => {
-  res.send('Usuario eliminado');
+  const { id } = req.params;
+  const response = deleteUserController(id);
+  res.status(200).send(response);
 }
 
 module.exports = {
