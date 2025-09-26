e5ce9da4c3cc5a69ad8662c351490a51d3633909
Dev-Leandr0 <leandroalegregabriel@outlook.com>
2025-09-26 20:37:32 -0300

    fix: corregir lógica en usersControllers

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -3,9 +3,50 @@ const users = require('../db/database');
 const createUserController = (name, username, email) => {
   const id = users.length + 1;
   const newUser = { id, name, username, email };
-  console.log(newUser);
+  if (!name || !username || !email) throw new Error(`Los datos están incompletos`);
+  users.push(newUser);
+  return newUser;
 };
+
+const getAllUsersController = () => {
+  return users;
+}
+
+const getUsersByNameController = (name) => {
+  const userByName = users.filter((user) => user.name === name);
+  if (!userByName.length) throw new Error(`No se encontró al usuario`);
+  return userByName;
+}
+
+const getOneUserByIdController = (id) => {
+  const userById = users.find((user) => user.id === Number(id));
+  if (!userById) throw new Error(`No se encontró el usuario con ese ID`);
+  return userById;
+}
+
+const updateUserController = (id, name, username, email) => {
+  const newUser = { name, username, email };
+  const userById = users.find((user) => user.id === Number(id));
+  if (userById) {
+    Object.assign(userById, newUser);
+  }
+  return userById;
+}
+
+const deleteUserController = (id) => {
+  const index = users.findIndex((user) => user.id === Number(id));
+  let deleteUser;
+  if (index !== -1) {
+    [deleteUser] = users.splice(index, 1);
+  }
+  return deleteUser;
+}
 
 module.exports = {
   createUserController,
+  getAllUsersController,
+  getUsersByNameController,
+  getOneUserByIdController,
+  updateUserController,
+  deleteUserController,
 }
